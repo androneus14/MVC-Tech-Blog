16de2b7bbd38be684850eb4b378602de51f49ede
Andy Nguyen <andynguyen_3@hotmail.com>
2022-06-03 20:12:57 +1000

    models for comment, post and user added

---
+++ models/Comment.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        comment_content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // We do this so people can't leave empty comments. Comments have to be at least one character long.
+            validate: {
+                len: [1]
+            }
+        },
+
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id',
+            }
+        },
+    },
+
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+);
+
+module.exports = Comment;
