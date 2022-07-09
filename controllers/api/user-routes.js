597a045d61651e6f57136922712eee5e1e00229a
Andy Nguyen <andynguyen_3@hotmail.com>
2022-07-09 17:46:10 +1000

    added main server.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -18,6 +18,7 @@ router.post('/', async (req, res) => {
     try {
         const userData = await User.create({
             username: req.body.username,
+            email: req.body.email,
             password: req.body.password
         });
 
