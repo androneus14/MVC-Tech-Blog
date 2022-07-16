11de4facd98605d284ad147a983be52c53455543
Andy Nguyen <andynguyen_3@hotmail.com>
2022-07-16 18:02:23 +1000

    edit hbs, edit + delete js added, subject to change

---
+++ public/js/edit-post.js
@@ -0,0 +1,26 @@
+const editFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const content = document.querySelector('textarea[name="post-content"]').value.trim();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch (`/api/post/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document
+    .querySelector('#edit-post-btn')
+    .addEventListener('click', editFormHandler);
